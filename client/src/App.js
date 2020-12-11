6c0eb443597e8a2adde7dbca8dc82e35c02872b7
Changmo Sung <changmosung0914@gmail.com>
2020-12-12 04:12:21 +0900

    setting up barebone for the project

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./components/styles/styles.scss";
+
+const App = () => {
+  return <div className="title">With U</div>;
+};
+
+export default App;
---
+++ models/Users.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+module.exports = Users = mongoose.model(
+  "users",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  })
+);
